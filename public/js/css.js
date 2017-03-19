2f738d9ebe834874dbf5f379e3dd8bdc5b4c58a1
antonabramov <anton@juniqe.com>
2017-03-19 16:48:44 +0100

    generate frontend files

---
+++ public/js/css.js
@@ -0,0 +1,12 @@
+var css =
+webpackJsonp_name_([0],{
+
+/***/ 2:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[2]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3NzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4uL3Njc3Mvc3R5bGVzLnNjc3MiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuLi9zY3NzL3N0eWxlcy5zY3NzXG4vLyBtb2R1bGUgaWQgPSAyXG4vLyBtb2R1bGUgY2h1bmtzID0gMCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7Ozs7Iiwic291cmNlUm9vdCI6IiJ9
